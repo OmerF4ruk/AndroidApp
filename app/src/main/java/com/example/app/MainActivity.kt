67d8c5e7d1227defaf898414999dced7a6c38364
OmerF4ruk <omer_Faruk18@outlook.com>
2023-01-05 19:37:38 +0300

    main Activity fragment_new_herb.xml e ayarlı onu değiştir.

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -7,7 +7,7 @@ import android.view.WindowManager
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_new_herb)
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
     }
 }
